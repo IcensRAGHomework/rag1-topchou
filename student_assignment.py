7752b7790ffb150fe37e978108b6309e23acc8a3
roro <102207431@nccu.edu.tw>
2025-01-22 20:33:43 +0800

    hw1

--- student_assignment.py
+++ student_assignment.py
@@ -1,6 +1,3 @@
-import json
-import traceback
-
 from model_configurations import get_model_configuration
 
 from langchain_openai import AzureChatOpenAI
@@ -9,9 +6,22 @@ from langchain_core.messages import HumanMessage
 gpt_chat_version = 'gpt-4o'
 gpt_config = get_model_configuration(gpt_chat_version)
 
+llm = AzureChatOpenAI(
+    model=gpt_config['model_name'],
+    deployment_name=gpt_config['deployment_name'],
+    openai_api_key=gpt_config['api_key'],
+    openai_api_version=gpt_config['api_version'],
+    azure_endpoint=gpt_config['api_base'],
+    temperature=gpt_config['temperature']
+)
+
+
 def generate_hw01(question):
-    pass
-    
+    response = llm.invoke([
+        HumanMessage(content=f"回答台灣 {question} 的紀念日有哪些，並使用 JSON 格式返回答案。")
+    ])
+
+    return response.content
 def generate_hw02(question):
     pass
     
@@ -21,20 +31,20 @@ def generate_hw03(question2, question3):
 def generate_hw04(question):
     pass
     
-def demo(question):
-    llm = AzureChatOpenAI(
-            model=gpt_config['model_name'],
-            deployment_name=gpt_config['deployment_name'],
-            openai_api_key=gpt_config['api_key'],
-            openai_api_version=gpt_config['api_version'],
-            azure_endpoint=gpt_config['api_base'],
-            temperature=gpt_config['temperature']
-    )
-    message = HumanMessage(
-            content=[
-                {"type": "text", "text": question},
-            ]
-    )
-    response = llm.invoke([message])
-    
-    return response
+# def demo(question):
+#     llm = AzureChatOpenAI(
+#             model=gpt_config['model_name'],
+#             deployment_name=gpt_config['deployment_name'],
+#             openai_api_key=gpt_config['api_key'],
+#             openai_api_version=gpt_config['api_version'],
+#             azure_endpoint=gpt_config['api_base'],
+#             temperature=gpt_config['temperature']
+#     )
+#     message = HumanMessage(
+#             content=[
+#                 {"type": "text", "text": question},
+#             ]
+#     )
+#     response = llm.invoke([message])
+#
+#     return response
